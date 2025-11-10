d79149d01e3ac164fa93be850b7a3ba3ddc66fc1
Chanin Inon <chanin.in@kubix.co>
2025-11-10 13:24:29 +0700

    feat: hello world

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.JSON(fiber.Map{
+			"message": "hello world",
+		})
 	})
 
 	app.Listen(":3000")
